69afcf0d337ae1fdb22da18df30228fda00245b2
Taylor Chen <s68175@gmail.com>
2020-09-18 00:01:47 +0800

    Add restaurant dashboard

--- controllers/restController.js
+++ controllers/restController.js
@@ -77,6 +77,17 @@ const restController = {
         })
       })
     })
+  },
+  getDashboard: (req, res) => {
+    return Restaurant.findByPk(req.params.id, {
+      include: [
+        Category,
+        { model: Comment, include: [User] }
+      ]
+    }).then(restaurant => {
+      console.log(restaurant.toJSON())
+      return res.render('dashboard', { restaurant: restaurant.toJSON() })
+    })
   }
 }
 
