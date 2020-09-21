b030e060099b37fb42383af5898723732982c1e2
Taylor Chen <s68175@gmail.com>
2020-09-21 22:48:12 +0800

    DELETE /api/admin/restaurants/:id

--- routes/apis.js
+++ routes/apis.js
@@ -7,5 +7,6 @@ const categoryController = require('../controllers/api/categoryController.js')
 router.get('/admin/restaurants', adminController.getRestaurants)
 router.get('/admin/restaurants/:id', adminController.getRestaurant)
 router.get('/admin/categories', categoryController.getCategories)
+router.delete('/admin/restaurants/:id', adminController.deleteRestaurant)
 
 module.exports = router
