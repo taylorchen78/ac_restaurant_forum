f164c35e50eb482f280b55d49b889ffa92511708
Taylor Chen <s68175@gmail.com>
2020-09-22 01:29:05 +0800

    PUT /api/admin/restaurants/:id

--- routes/apis.js
+++ routes/apis.js
@@ -11,6 +11,7 @@ router.get('/admin/restaurants', adminController.getRestaurants)
 router.get('/admin/restaurants/:id', adminController.getRestaurant)
 router.get('/admin/categories', categoryController.getCategories)
 router.post('/admin/restaurants', upload.single('image'), adminController.postRestaurant)
+router.put('/admin/restaurants/:id', upload.single('image'), adminController.putRestaurant)
 router.delete('/admin/restaurants/:id', adminController.deleteRestaurant)
 
 module.exports = router
