8d723139e9f0a3f8d4007ec2c76fef542a439b95
Taylor Chen <s68175@gmail.com>
2020-09-22 01:11:40 +0800

    POST /api/admin/restaurants

--- routes/apis.js
+++ routes/apis.js
@@ -1,12 +1,16 @@
 const express = require('express')
 const router = express.Router()
 
+const multer = require('multer')
+const upload = multer({ dest: 'temp/' })
+
 const adminController = require('../controllers/api/adminController.js')
 const categoryController = require('../controllers/api/categoryController.js')
 
 router.get('/admin/restaurants', adminController.getRestaurants)
 router.get('/admin/restaurants/:id', adminController.getRestaurant)
 router.get('/admin/categories', categoryController.getCategories)
+router.post('/admin/restaurants', upload.single('image'), adminController.postRestaurant)
 router.delete('/admin/restaurants/:id', adminController.deleteRestaurant)
 
 module.exports = router
--- services/adminService.js
+++ services/adminService.js
@@ -1,3 +1,6 @@
+const imgur = require('imgur-node-api')
+const IMGUR_CLIENT_ID = process.env.IMGUR_CLIENT_ID
+
 const db = require('../models')
 const Restaurant = db.Restaurant
 const Category = db.Category
@@ -21,6 +24,41 @@ const adminService = {
             callback({ status: 'success', message: '' })
           })
       })
+  },
+  postRestaurant: (req, res, callback) => {
+    if (!req.body.name) {
+      return callback({ status: 'error', message: "name didn't exist" })
+    }
+
+    const { file } = req // equal to const file = req.file
+    if (file) {
+      imgur.setClientID(IMGUR_CLIENT_ID)
+      imgur.upload(file.path, (err, img) => {
+        return Restaurant.create({
+          name: req.body.name,
+          tel: req.body.tel,
+          address: req.body.address,
+          opening_hours: req.body.opening_hours,
+          description: req.body.description,
+          image: file ? img.data.link : null,
+          CategoryId: req.body.categoryId
+        }).then((restaurant) => {
+          callback({ status: 'success', message: 'restaurant was successfully created' })
+        })
+      })
+    } else {
+      return Restaurant.create({
+        name: req.body.name,
+        tel: req.body.tel,
+        address: req.body.address,
+        opening_hours: req.body.opening_hours,
+        description: req.body.description,
+        CategoryId: req.body.categoryId
+      })
+        .then((restaurant) => {
+          callback({ status: 'success', message: 'restaurant was successfully created' })
+        })
+    }
   }
 }
 
